d6907cb71fdbe7c7bd24bfee41db215e7326e4f4
damcurry <1519052177@qq.com>
2022-02-28 23:15:03 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'
 
 createApp(App).use(router).mount('#app')
